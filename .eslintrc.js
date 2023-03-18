97c77ecaa7403f68141333d36c93e350aa399f85
wudalei <tyml777777789@qq.com>
2023-03-18 17:13:11 +0800

    feat: 增加环境变量文件。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: wudalei
+ * @LastEditor: wudalei
+ * @Descripttion:
+ * @params:
+ * @Date: 2023-03-16 16:41:44
+ * @LastEditTime: 2023-03-18 14:02:21
+ */
 module.exports = {
   extends: [require.resolve('@umijs/lint/dist/config/eslint')],
   globals: {
